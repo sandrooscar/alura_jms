7ed27f077bff7df3fb1a431a9ad520d50bbd4c71
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2020-04-04 19:59:33 -0300

    AULA02-Consumindo mensagens JMS-Agora consumindo a mensagem do activeMQ

--- jms/src/br/com/caelum/jms/TesteConsumidor.java
+++ jms/src/br/com/caelum/jms/TesteConsumidor.java
@@ -4,29 +4,45 @@ import java.util.Scanner;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
 import javax.naming.InitialContext;
 
 public class TesteConsumidor {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
-		//inicializa o context JNI
-		InitialContext context = new InitialContext();
-		
 //		Properties props = new Properties(); 
 //		props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory"); 
 //		props.setProperty(Context.PROVIDER_URL, "vm://localhost:8161"); 
 //		javax.naming.Context context = new InitialContext(props);
-//		
+
+		//inicializa o context JNI
+		InitialContext context = new InitialContext();
 		//pega a fabrica configurada no jni
 		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		
 		//cria a conexao com o activemq
 		Connection connection = factory.createConnection();
 		//inicia a conexão
 		connection.start();
 		
+		//cria a session para interagir com a fila
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		//criando o consumidor, utilizando o lookup financeiro (queue.financeiro) do arquivo jndi.properties
+		Destination file = (Destination) context.lookup("financeiro");
+		MessageConsumer consumer = session.createConsumer(file);
+		
+		//consumindo a mensagem do activeMQ
+		Message message = consumer.receive();
+		
+		System.out.println("Recebendo msg: "+message);
 		new Scanner(System.in).nextLine();
 		
+		session.close();
 		connection.close();
 		context.close();
 	}
