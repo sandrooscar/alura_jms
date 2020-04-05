5f9af8bee2c8c0b3b3cf04e36e823e565a335638
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2020-04-05 14:44:29 -0300

    AULA05-Topicos e assinaturas duráveis-Consumidor (ainda não durável)-Só recebe quando online

---
+++ jms/src/br/com/caelum/topic/broadcast/TesteProdutorTopic.java
@@ -0,0 +1,47 @@
+package br.com.caelum.topic.broadcast;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+
+public class TesteProdutorTopic {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws Exception {
+//		Properties props = new Properties(); 
+//		props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory"); 
+//		props.setProperty(Context.PROVIDER_URL, "vm://localhost:8161"); 
+//		javax.naming.Context context = new InitialContext(props);
+
+		//inicializa o context JNI
+		InitialContext context = new InitialContext();
+		//pega a fabrica configurada no jni
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		
+		//cria a conexao com o activemq
+		Connection connection = factory.createConnection();
+		//inicia a conexão
+		connection.start();
+		
+		//cria a session para interagir com a fila
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		//criando o produtor, utilizando o lookup financeiro (queue.financeiro) do arquivo jndi.properties
+		Destination topico = (Destination) context.lookup("loja");
+		MessageProducer producer = session.createProducer(topico);
+		
+		for (int i = 0; i < 50; i++) {
+			Message message = session.createTextMessage("<pedido><id>"+i+"</id></pedido>");
+			producer.send(message);
+		}
+		
+		session.close();
+		connection.close();
+		context.close();
+	}
+
+}
