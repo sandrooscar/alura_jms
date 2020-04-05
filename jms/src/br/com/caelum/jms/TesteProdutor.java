341b7700f0769c71c4d56d98f05fccc6ebb63e1e
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2020-04-05 09:18:44 -0300

    AULA04-Enviando mensagens e competição entre consumidores

--- jms/src/br/com/caelum/jms/TesteProdutor.java
+++ jms/src/br/com/caelum/jms/TesteProdutor.java
@@ -7,13 +7,13 @@ import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
-public class TesteConsumidor {
+public class TesteProdutor {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
@@ -37,24 +37,12 @@ public class TesteConsumidor {
 		
 		//criando o consumidor, utilizando o lookup financeiro (queue.financeiro) do arquivo jndi.properties
 		Destination fila = (Destination) context.lookup("financeiro");
-		MessageConsumer consumer = session.createConsumer(fila);
+		MessageProducer producer = session.createProducer(fila);
 		
-		//adicionando listener para receber uma ou mais mensagens do tipo texto
-		consumer.setMessageListener(new MessageListener(){
-
-			@Override
-			public void onMessage(Message message) {
-				TextMessage textMessage = (TextMessage)message;
-				try {
-					System.out.println("Message Listener - Recebendo mensagem: "+textMessage.getText());
-				} catch (JMSException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		});
-		
-		new Scanner(System.in).nextLine();
+		for (int i = 0; i < 5000; i++) {
+			Message message = session.createTextMessage("<pedido><id>"+i+"</id></pedido>");
+			producer.send(message);
+		}
 		
 		session.close();
 		connection.close();
