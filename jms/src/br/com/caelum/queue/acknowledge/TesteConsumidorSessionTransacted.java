7d4dcc4b2d258e1cb23000b170f28cd7ce4dc0fb
Sandro Oscar Bugmann <prof.sandrob@gmail.com>
2020-04-10 15:24:28 -0300

    AUA07-Enviando mensagens especificas e tratamento de erro- Tipos de acknowledge

--- jms/src/br/com/caelum/queue/acknowledge/TesteConsumidorSessionTransacted.java
+++ jms/src/br/com/caelum/queue/acknowledge/TesteConsumidorSessionTransacted.java
@@ -1,4 +1,4 @@
-package br.com.caelum.jms;
+package br.com.caelum.queue.acknowledge;
 
 import java.util.Scanner;
 
@@ -7,19 +7,20 @@ import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
+import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
-import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
-public class TesteProdutor {
+public class TesteConsumidorSessionTransacted {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
 //		Properties props = new Properties(); 
 //		props.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.activemq.jndi.ActiveMQInitialContextFactory"); 
 //		props.setProperty(Context.PROVIDER_URL, "vm://localhost:8161"); 
+//		properties.setProperty("queue.financeiro", "fila.financeiro");
 //		javax.naming.Context context = new InitialContext(props);
 
 		//inicializa o context JNI
@@ -34,16 +35,29 @@ public class TesteProdutor {
 		connection.start();
 		
 		//cria a session para interagir com a fila
-		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Session session = connection.createSession(true, Session.SESSION_TRANSACTED);
 		
-		//criando o produtor, utilizando o lookup financeiro (queue.financeiro) do arquivo jndi.properties
+		//criando o consumidor, utilizando o lookup financeiro (queue.financeiro) do arquivo jndi.properties
 		Destination fila = (Destination) context.lookup("financeiro");
-		MessageProducer producer = session.createProducer(fila);
+		MessageConsumer consumer = session.createConsumer(fila);
 		
-		for (int i = 0; i < 5; i++) {
-			Message message = session.createTextMessage("<pedido><id>"+i+"</id></pedido>");
-			producer.send(message);
-		}
+		//adicionando listener para receber uma ou mais mensagens do tipo texto
+		consumer.setMessageListener(new MessageListener(){
+
+			@Override
+			public void onMessage(Message message) {
+				TextMessage textMessage = (TextMessage)message;
+				try {
+					System.out.println("Message Listener - Recebendo mensagem: "+textMessage.getText());
+					session.commit();
+				} catch (JMSException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		new Scanner(System.in).nextLine();
 		
 		session.close();
 		connection.close();
